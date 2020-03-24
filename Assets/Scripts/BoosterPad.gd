6876a6085cb27fe5e8007e265ccd6602c97c4498
jasperro <jasper@albering.nl>
2020-03-24 13:17:09 +0100

    Booster Pad

---
+++ Assets/Scripts/BoosterPad.gd
@@ -0,0 +1,10 @@
+extends Area
+
+const VEL = 200.0
+
+func _on_BoosterPad_area_entered(area):
+	var egg = area.get_parent()
+	#var vel = Vector2(egg.get_linear_velocity().x, egg.get_linear_velocity().z)
+	print(egg.get_linear_velocity())
+	egg.set_linear_velocity(get_global_transform().basis * Vector3(1,-30,30))
+	#egg.set_linear_velocity(get_global_transform().basis * Vector3(1,0,-1))
